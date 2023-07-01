38e2cca998a47ce4e796ce9501d70531290351c4
yawai990 <ywaung990@gmail.com>
2023-07-01 21:19:32 +0630

    aws set up fixed

--- awsapacheserversetup.sh
+++ awsapacheserversetup.sh
@@ -2,9 +2,11 @@ sudo yum update -y
 echo "update completed"
 echo "httpd installation process started"
 sudo yum install httpd -y
-sudo yum start httpd 
-sudo yum enable httpd
+sudo systemctl start httpd 
+sudo systemctl enable httpd.httpd.service
 echo "httpd installation,starting,enabling completed"
 echo "writing index.html file"
 echo "<h1>Hello world $(hostname -f)</h1>" > /var/www/html/index.html
 echo "index.html file created successfully"
+sudo systemctl restart httpd.service
+echo "httpd set up completed'
